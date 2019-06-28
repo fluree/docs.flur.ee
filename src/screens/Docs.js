2dcc3cb3cc30cbbb2cbe22fce1f144467d05df7c
Andrew Johnson <ajohnson.uva@gmail.com>
2019-06-28 13:47:24 -0400

    add toast notifications when switching languages

--- src/screens/Docs.js
+++ src/screens/Docs.js
@@ -4,6 +4,7 @@ import get from 'lodash.get';
 import { SidebarNav, fixSidebar, getTopicAndSubTopic, getNextTopic, getPreviousTopic } from '../components/LoadTopics';
 import marked from 'marked';
 import { Search } from './DocIndex';
+import Notifications, { notify } from 'react-notify-toast'
 
 export const docNav = {
     "getting-started": {
@@ -221,7 +222,8 @@ class Docs extends React.Component {
         displaySearch: false,
         searchValue: "",
         hashAnchor: "",
-        scrollElementId: ""
+        scrollElementId: "",
+        showLanguageToast: false
     }
 
     
@@ -369,6 +371,8 @@ class Docs extends React.Component {
 
     changeLanguage = (html, language) => {
         html = this.loadLanguage(html, language)
+        var languageText = { flureeql: "FlureeQL", graphql: "GraphQL", curl: "Curl", sparql: "SPARQL"}
+        notify.show(`Coding Examples Now In ${languageText[language]}`, "success", 1500)
         this.setState({language: language, markdown: html, scrollElementId: this.setScrollElementId()})
     }
 
@@ -380,7 +384,7 @@ class Docs extends React.Component {
         const { markdown, headers, headerLinks, topic, subtopic, language, previousTopic, nextTopic,  fixedSidebar, displaySearch } = this.state;
         return(
             <div className="row">
-
+                    <Notifications />
                     <div className="col-md-4 mt20 mb20">
                     <div className={fixedSidebar ? "fixedSidebar" : "sidebar" }>
                             <div>
