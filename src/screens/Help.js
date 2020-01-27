b2fdd34550191e5ffd5bd11afb6ae0b23b5c067e
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-01-27 09:38:20 -0500

    FC-255 Update Help Page with new UI components.

--- src/screens/Help.js
+++ src/screens/Help.js
@@ -1,44 +1,118 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
 
 class Help extends React.Component {
     render() {
         return (
-            <div className="text-center mt20">
-            <div className="row">
-                <div className="col-xs-12">
-                    <h2>Need Help?</h2>
+            <div style={{
+                height: "100vh"
+            }}>
+                <h2 style={{
+                    textAlign: "center",
+                    marginBottom: "20px"
+                }}>Need Help?</h2>
+                <div
+                    style={{
+                            display: "flex",
+                            justifyContent: "space-around"
+                        }}>
+                    <div 
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            border: "solid 1px #ccc",
+                            boxShadow: "2px 2px  3px  #ccc",
+                            borderRadius: "4px",
+                            padding: "0 15px 0 15px",
+                            width: "35%"
+                        }}>
+                        <div className="image-haeder"
+                            style={{
+                                textAlign: "center"
+                            }}>
+                            <img
+                             style={{
+                                width: "200px",
+                                height: "150px"
+                            }}
+                             alt="Contact Image" src={require('../theme/assets/contactImage.png')}/>
+                        </div>
+                       <h3 
+                        style={{
+                            textAlign: "center"
+                        }}
+                        >Contact</h3>
+                       <p 
+                        style={{
+                            textAlign: "center",
+                            margin:"10px 0 20px 0"
+                        }}>Email us with any questions, comments, or concerns.</p> 
+                        <div
+                            style={{
+                                textAlign: "center"
+                            }}>
+                             <a
+                             style={{
+                            width: "75%"
+                        }}
+                        className="buttonPurple no-underline"
+                             href="mailto:support@flur.ee">Send Us An Email</a>
+                        </div>
+                          
+                    </div>
+                    <div
+                         style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            width: "35%",
+                            justifyContent: "center",
+                            border: "solid 1px #ccc",
+                            boxShadow: "2px 2px  3px  #ccc",
+                            borderRadius: "4px",
+                            padding: "0 15px 30px 15px",
+                        }}>
+                          <div
+                          className="image-header"
+                            style={{
+                                textAlign: "center"
+                            }}>
+                            <img
+                             style={{
+                                width: "200px",
+                                height: "125px"
+                            }}
+                             alt="Contact Image" src={require('../theme/assets/slackimage.png')}/>
+                        </div>
+                        <h3
+                        style={{
+                            textAlign: "center",
+                            marginTop: "10px"
+                        }}
+                        >Join Fluree Slack</h3>
+                        <p
+                        style={{
+                            textAlign: "center",
+                            margin:"10px 0 30px 0"
+                        }}>Meet Fluree developers, ask quesitons, and help others when you join our Slack Channel</p>
+                        <div style={{
+                            textAlign: "center"
+                        }}>
+                            <a className="no-underline" href="https://launchpass.com/flureedb" 
+                                rel="noopener noreferrer" target="_blank"
+                                style={{
+                            width: "75%"
+                        }}
+                        className="buttonPurple no-underline">
+                                Join Our Slack Channel 
+                                <i style={{
+                                    marginLeft: "4px"
+                                }} className="fab fa-slack"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="row text-center">
-                <div className="col-sm-6 text-center">
-                        <div className="lesson-square">
-                            <div className="lesson-square-icon">
-                                <i className="fas fa-at"/>
-                            </div>
-                            <div className="lesson-square-header">
-                            support@flur.ee</div>
-                            <div className="lesson-square-description">
-                            Email us with any questions, comments, or concerns.
-                            </div>
-                        </div>
-                </div>
-                <div className="col-sm-6 text-center">
-                    <a className="no-underline" href="https://launchpass.com/flureedb" 
-                    rel="noopener noreferrer" target="_blank">
-                        <div className="lesson-square beginner">
-                            <div className="lesson-square-icon">
-                                <i className="fab fa-slack"/>
-                            </div>
-                            <div className="lesson-square-header">
-                            Join Our Slack</div>
-                            <div className="lesson-square-description">
-                            Enter your email address to get a Slack invitation. 
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
         )
     }
 }
