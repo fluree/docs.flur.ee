5937ced5fb2c794dc20e21252083a4f10b8378c9
Tr3yb0 <tbotard@flur.ee>
2021-10-15 17:17:01 -0400

    Add content, styling, and icons to community page

--- website/src/pages/community/index.tsx
+++ website/src/pages/community/index.tsx
@@ -3,69 +3,58 @@ import Layout from '@theme/Layout';
 import CommunityCard from './CommunityCard';
 import styles from './Community.module.css';
 
-const CommunityList = [
+export interface CommunityDetails {
+  title: string,
+  svgPath: string,
+  svgPathDark?: string,
+  description: string,
+  link: string
+}
+
+const CommunityList: CommunityDetails[] = [
+{
+  title: 'Github',
+  svgPath: '/img/github-icon-light.svg',
+  svgPathDark: '/img/github-icon-dark.svg',
+  description: 'Where all the magic happens. All our source code lives here. Come check out our repos, projects, and get involved!',
+  link: 'https://github.com/fluree'
+},
   {
-    title: 'Github Discussions',
-    Svg: require('../../../static/img/github-icon-light.svg').default,
-    description: (
-      <>
-        Long-lived conversations about Fluree, feature requests, ideas, Fluree love, etc. 
-      </>
-    ),
+    title: 'Community Forum',
+    svgPath: '/img/forum.svg',
+    description: 'Long-lived conversations about Fluree, feature requests, ideas, Fluree love, etc.',
     link: 'https://github.com/fluree/db/discussions'
   },
   {
-    title: 'Fluree Slack',
-    Svg: require('../../../static/img/Slack_Mark.svg').default,
-    description: (
-      <>
-        Come join our community members and Fluree teammates for realtime chat. 
-      </>
-    ),
+    title: 'Slack',
+    svgPath: '/img/Slack_Mark.svg',
+    description: 'Come join our community members and Fluree teammates for realtime chat. ',
     link: 'https://launchpass.com/flureedb'
   },
   {
     title: 'YouTube',
-    Svg: require('../../../static/img/youtube-icon.svg').default,
-    description: (
-      <>
-        All the Fluree video content lives on our Youtube. Come check it out. 
-      </>
-    ),
+    svgPath: '/img/youtube-icon.svg',
+    description: 'All the Fluree video content lives on our Youtube. Come check out a webinar, walkthrough. ',
     link: 'https://youtube.com/c/fluree'
   },
   {
-    title: 'Github',
-    Svg: require('../../../static/img/github-icon-light.svg').default,
-    description: (
-      <>
-        Where all the magic happens. All our source code lives here. Come check out our repos, projects, and get involved!
-      </>
-    ),
-    link: 'https://github.com/fluree'
-  },
-  {
     title: 'Reddit',
-    Svg: require('../../../static/img/Reddit_Mark_OnWhite.svg').default,
-    description: (
-      <>
-        Join us on the r/Fluree reddit page.   
-      </>
-    ),
+    svgPath: '/img/Reddit_Mark_OnWhite.svg',
+    description: 'Join us on the r/Fluree reddit page!',
     link: 'https://www.reddit.com/r/Fluree/'
   }
 ];
 
-export default function Community(){
+export default function Community(): JSX.Element {
   return (
     <Layout>
       <main className={styles.community}>
         <div className="container">
-          <div className="row">
+          <section className="row">
             {CommunityList.map((props, idx) => (
               <CommunityCard key={idx} {...props} />
             ))}
-          </div>
+          </section>
         </div>
       </main>
     </Layout>
