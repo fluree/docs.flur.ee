819aead9905fb4f69a831dc4096e3142a78b8189
Flor Marshall <flordmarshall@gmail.com>
2021-10-20 16:38:39 -0400

    create Card element

---
+++ website/src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Card({ title, description, href }) {
+   <div className="card-demo">
+  <div className="card">
+    <div className="card__header">
+      <h3>{title}</h3>
+    </div>
+    <div className="card__body">
+      <p>
+{description}
+      </p>
+    </div>
+    <div className="card__footer">
+      <a href='' className="button button--secondary button--block">See All</a>
+    </div>
+  </div>
+</div>
+}
