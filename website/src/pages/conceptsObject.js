b59a241fddf6dc56a2f9f84daa6cedba8e5bf18a
Flor Marshall <flordmarshall@gmail.com>
2021-10-25 12:40:56 -0400

    create concepts file for object

---
+++ website/src/pages/conceptsObject.js
@@ -0,0 +1,28 @@
+const ConceptCardDetails = [
+  {
+    title: 'Network Infrastructure',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    link: '/docs/concepts/infrastructure/network_infrastructure/',
+  },
+  {
+    title: 'Auth',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    link: '/docs/concepts/identity/auth_records/',
+  },
+  {
+    title: 'Smart Functions',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    link: '/docs/concepts/smart-functions/smartfunctions/',
+  },
+  {
+    title: 'Analytical Queries',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    link: '/docs/concepts/analytical-queries/inner-joins-in-fluree/',
+  },
+];
+
+export default ConceptCardDetails;
