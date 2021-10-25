8cdf8b9e3aa685876ac8b1fc2af0088a6331eeb7
Flor Marshall <flordmarshall@gmail.com>
2021-10-25 12:39:33 -0400

    create Card component

---
+++ website/src/components/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from '@docusaurus/Link';
+import {CardDetails} from '../components/CardDetails.interface'
+
+
+export default function Card({ title, description, link }: CardDetails): JSX.Element {
+  return(
+  <>
+   <Link to={link} className="button button--secondary button--block box__a margin--md">
+     <div className="card-demo">
+  <div className="card">
+    <div className="card__header">
+      <h3>{title}</h3>
+    </div>
+    <div className="card__body text__body">
+      <p>
+      {description}
+      </p>
+    </div>
+    <div className="card__footer">
+    </div>
+  </div>
+    </div>
+    </Link>
+    </>
+);
+}
