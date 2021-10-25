1ad8d7bfcd3f1c9b5877328df9c9e51308b9f1c2
Flor Marshall <flordmarshall@gmail.com>
2021-10-25 12:40:41 -0400

    remove unnecesary code

--- website/src/components/CardGrid.tsx
+++ website/src/components/CardGrid.tsx
@@ -1,59 +1,16 @@
 import React from 'react';
-import Link from '@docusaurus/Link';
+import Card from './Card';
 
-const CardDetails = [
-  {
-    title: 'Network Infrastructure',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    to: '/docs/concepts/infrastructure/network_infrastructure/',
-  },
-  {
-    title: 'Auth',  
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    to: '/docs/concepts/identity/auth_records/'
-  },
-  {
-    title: 'Smart Functions',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    to: '/docs/concepts/smart-functions/smartfunctions/'
-  },
-  {
-    title: 'Analytical Queries',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    to: '/docs/concepts/analytical-queries/inner-joins-in-fluree/'
-  },
-]
 
-const Card = ({ title, description, to }) => {
-  return(
-  <>
-   <Link to={to} className="button button--secondary button--block box__a margin--md">
-     <div className="card-demo">
-  <div className="card">
-    <div className="card__header">
-      <h3>{title}</h3>
-    </div>
-    <div className="card__body text__body">
-      <p>
-      {description}
-      </p>
-    </div>
-    <div className="card__footer">
-    </div>
-  </div>
-    </div>
-    </Link>
-    </>
-);
-}
-
-export default function CardGrid() {
+export default function CardGrid({ Object }) {
   return (
     <section>
       <div className='container'>
         <div className='col col--6'>
           <div className='row'>
-            <Card title description to ></Card>
+            { Object.map((props, index) => (
+              <Card key={index} {...props} />
+            ))}
           </div>
         </div>
       </div>
