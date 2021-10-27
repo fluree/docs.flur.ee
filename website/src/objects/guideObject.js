af8df9a919cbc481926d8b744dc555677e963b58
Flor Marshall <flordmarshall@gmail.com>
2021-10-27 16:36:18 -0400

    set up the objects

---
+++ website/src/objects/guideObject.js
@@ -0,0 +1,26 @@
+export const GuideCardDetails = [
+  {
+    title: 'Queries',
+    description:
+      "Understanding Fluree's network infrastructure, different server types, query peers and transactor groups, as well as Consensus algorithms when running a decentralized Fluree.",
+    link: '/docs/concepts/infrastructure/network_infrastructure/',
+  },
+  {
+    title: 'Flakes and Blocks',
+    description:
+      'How to generate a Public-Private Key, understand the relationship of the different collections that enable persmissioning (Auth, Role, and Rule), and other features like the Authority model.',
+    link: '/docs/concepts/identity/auth_records/',
+  },
+  {
+    title: 'Smart Functions',
+    description:
+      'Learn how to create a smart function, their role in setting permissions in Fluree, where they are stored within Fluree and how they are referenced, and resources to help build a smart function.',
+    link: '/docs/concepts/smart-functions/smartfunctions/',
+  },
+  {
+    title: 'Tools',
+    description:
+      'How inner joins work in Fluree, using Full Text Search with analytical queries, how to optimize your analytical queries with the available optional clauses.',
+    link: '/docs/concepts/analytical-queries/inner-joins-in-fluree/',
+  },
+];
---
+++ website/src/objects/referenceObject.js
@@ -0,0 +1,26 @@
+export const RefCardDetails = [
+  {
+    title: 'Startup Config',
+    description:
+      "Understanding Fluree's network infrastructure, different server types, query peers and transactor groups, as well as Consensus algorithms when running a decentralized Fluree.",
+    link: '/docs/reference/fluree_config/',
+  },
+  {
+    title: 'HTTP endpoints',
+    description:
+      'How to generate a Public-Private Key, understand the relationship of the different collections that enable persmissioning (Auth, Role, and Rule), and other features like the Authority model.',
+    link: '/docs/reference/http/overview/',
+  },
+  {
+    title: 'Clojure',
+    description:
+      'Learn how to create a smart function, their role in setting permissions in Fluree, where they are stored within Fluree and how they are referenced, and resources to help build a smart function.',
+    link: '/docs/reference/clojure/',
+  },
+  {
+    title: 'Crypto',
+    description:
+      'How inner joins work in Fluree, using Full Text Search with analytical queries, how to optimize your analytical queries with the available optional clauses.',
+    link: '/docs/reference/crypto/',
+  },
+];
