dd2a9140c38445987f12a9f86b4cb74d2b5fe2d3
Flor Marshall <flordmarshall@gmail.com>
2021-10-27 10:21:53 -0400

    change col -> row

--- website/src/components/CardGrid.tsx
+++ website/src/components/CardGrid.tsx
@@ -5,7 +5,7 @@ import Card from './Card';
 export default function CardGrid({ cardDetails }) {
   return (
     <section className='container'>
-        <div className='col'>
+        <div className='row'>
           <div className='row'>
             {cardDetails.map((props, index) => (
               <Card key={index} {...props} />
