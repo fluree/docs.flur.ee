c772668f63803780d08fc3151e0e4a2949d3e0fc
Tr3yb0 <tbotard@flur.ee>
2021-10-28 15:50:05 -0400

    Add learn more section to landing page

--- website/src/components/home_card/HomepageCardDetails.ts
+++ website/src/components/home_card/HomepageCardDetails.ts
@@ -4,31 +4,37 @@ export const HomepageCardDetails: CardDetails[] = [
     {
         title: 'SmartFunctions',
         description: 'This is the SmartFunctions Card',
-        link: '/docs/guides/advanced/smart-functions/1'
+        link: '/docs/guides/advanced/smart-functions/1',
+        Svg: require('../../../static/img/heroicons/acadmic-cap.svg').default
     },
     {
         title: 'Analytical Queries',
         description: 'This is the Analytical Queries Card',
-        link: '/docs/guides/querying/query-advanced/5/'
+        link: '/docs/guides/querying/query-advanced/5/',
+        Svg: require('../../../static/img/heroicons/search-circle.svg').default
     },
     {
         title: 'Fluree Architecture',
         description: 'This is the Fluree Architecture Card',
-        link: '/docs/concepts/architecture/flakes/'
+        link: '/docs/concepts/architecture/flakes/',
+        Svg: require('../../../static/img/heroicons/library.svg').default
     },
     {
         title: 'Transactions',
         description: 'This is the Transactions Card',
-        link: '/docs/overview/transact/basics/'
+        link: '/docs/overview/transact/basics/',
+        Svg: require('../../../static/img/heroicons/shield-check.svg').default
     },
     {
         title: 'APIs',
         description: 'This is the APIs Card',
-        link: '/docs/reference/http/overview/'
+        link: '/docs/reference/http/overview/',
+        Svg: require('../../../static/img/heroicons/switch-horizontal.svg').default
     },
     {
         title: 'Schemas',
         description: 'This is the Schemas Card',
-        link: '/docs/guides/schema/1/'
+        link: '/docs/guides/schema/1/',
+        Svg: require('../../../static/img/heroicons/book-open.svg').default
     }
 ];
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -7,48 +7,41 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
-import HomepageCard from '../components/home_card/HomepageCard';
-import { HomepageCardDetails } from '../components/home_card/HomepageCardDetails';
+import HomepageCards from '../components/home_card/HomepageCards';
 
 const HomepageHeader = () => {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">Fluree for Developers</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons} >
-          <Link
-            className="button button--secondary margin-horiz--lg"
-            to="/docs/overview/getting_started">
-            Get Started
-          </Link>
-          <Link
-            className="button button--secondary margin-horiz--lg"
-            to="/docs/overview/fluree_basics">
-            Fluree Basics
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx('hero hero--primary', styles.heroBanner)}>
+			<div className="container">
+				<h1 className="hero__title">Fluree for Developers</h1>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div className={styles.buttons} >
+					<Link
+						className="button button--secondary margin-horiz--lg"
+						to="/docs/overview/getting_started">
+						Get Started
+					</Link>
+					<Link
+						className="button button--secondary margin-horiz--lg"
+						to="/docs/overview/fluree_basics">
+						Fluree Basics
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description={`${siteConfig.tagline}`}>
-      <HomepageHeader />
-      <section className='container homepage-cards'>
-        <div className='row'>
-          {HomepageCardDetails.map((props, idx) => (
-            <HomepageCard key={idx} {...props} />
-          ))}
-        </div>
-        <HomepageFeatures />
-      </section>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={`${siteConfig.title}`}
+			description={`${siteConfig.tagline}`}>
+			<HomepageHeader />
+			<HomepageCards />
+			{/* <HomepageFeatures /> */}
+		</Layout>
+	);
 }
