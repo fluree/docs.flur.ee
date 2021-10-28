ce86042ea31bfc107414615124b93efd52b96963
Flor Marshall <flordmarshall@gmail.com>
2021-10-28 14:28:58 -0400

    edit ref object

--- website/src/objects/referenceObject.js
+++ website/src/objects/referenceObject.js
@@ -2,25 +2,25 @@ export const RefCardDetails = [
   {
     title: 'Startup Config',
     description:
-      "Understanding Fluree's network infrastructure, different server types, query peers and transactor groups, as well as Consensus algorithms when running a decentralized Fluree.",
+      "Understand and make changes to Fluree's configurable properties like Base settings, Transactor groups, HTTP API settings, Decentralized Ledger settings and Password and JWT settings.",
     link: '/docs/reference/fluree_config/',
   },
   {
     title: 'HTTP endpoints',
     description:
-      'How to generate a Public-Private Key, understand the relationship of the different collections that enable persmissioning (Auth, Role, and Rule), and other features like the Authority model.',
+      'Learn how to leverage all the available endpoints such as Main endpoints, Test endpoints, Password Authentication endpoints, and others. Along with example for each.',
     link: '/docs/reference/http/overview/',
   },
   {
     title: 'Clojure',
     description:
-      'Learn how to create a smart function, their role in setting permissions in Fluree, where they are stored within Fluree and how they are referenced, and resources to help build a smart function.',
+      "Access Fluree's Clojure Client, learn setup and async modifications, how to transact schema and data, and issue different types of queries like analytical queries, historical queries, and block queries.",
     link: '/docs/reference/clojure/',
   },
   {
     title: 'Crypto',
     description:
-      'How inner joins work in Fluree, using Full Text Search with analytical queries, how to optimize your analytical queries with the available optional clauses.',
+      'Issue signed queries and transactions in closed-api-mode by using the crypto-utils and crypto-base JS librares. For the ClojureScript use the fluree.crypto library.',
     link: '/docs/reference/crypto/',
   },
 ];
